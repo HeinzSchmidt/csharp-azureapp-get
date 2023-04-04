26835d76da647b2a3bd340d826db66195e6b07d9
Heinz Schmidt <heinz.schmidt@wdtrust.org.uk>
2023-04-04 15:41:21 +0100

    Initial

---
+++ getcs.cs
@@ -0,0 +1,70 @@
+using (var client = new HttpClient())
+{
+    var request = new HttpRequestMessage()
+    {
+        RequestUri = new Uri("https://az-uks-sbx-det111-appconfig.azconfig.io/kv?api-version=1.0"),
+        Method = HttpMethod.Get
+    };
+
+    //
+    // Sign the request
+    request.Sign("NQer-lo-s0:sFCuLdkNmLPQiJKejVB1", "l1wZTfxZCLSbuiz/t1FHZKWQYkcNGIVofctHYkNHPS0=");
+
+    await client.SendAsync(request);
+}
+
+static class HttpRequestMessageExtensions
+{
+    public static HttpRequestMessage Sign(this HttpRequestMessage request, string credential, string secret)
+    {
+        string host = request.RequestUri.Authority;
+        string verb = request.Method.ToString().ToUpper();
+        DateTimeOffset utcNow = DateTimeOffset.UtcNow;
+        string contentHash = Convert.ToBase64String(request.Content.ComputeSha256Hash());
+
+        //
+        // SignedHeaders
+        string signedHeaders = "date;host;x-ms-content-sha256"; // Semicolon separated header names
+
+        //
+        // String-To-Sign
+        var stringToSign = $"{verb}\n{request.RequestUri.PathAndQuery}\n{utcNow.ToString("r")};{host};{contentHash}";
+
+        //
+        // Signature
+        string signature;
+
+        using (var hmac = new HMACSHA256(Convert.FromBase64String(secret)))
+        {
+            signature = Convert.ToBase64String(hmac.ComputeHash(Encoding.ASCII.GetBytes(stringToSign)));
+        }
+
+        //
+        // Add headers
+        request.Headers.Date = utcNow;
+        request.Headers.Add("x-ms-content-sha256", contentHash);
+        request.Headers.Authorization = new AuthenticationHeaderValue("HMAC-SHA256", $"Credential={credential}&SignedHeaders={signedHeaders}&Signature={signature}");
+
+        return request;
+    }
+}
+
+static class HttpContentExtensions
+{
+    public static byte[] ComputeSha256Hash(this HttpContent content)
+    {
+        using (var stream = new MemoryStream())
+        {
+            if (content != null)
+            {
+                content.CopyToAsync(stream).Wait();
+                stream.Seek(0, SeekOrigin.Begin);
+            }
+
+            using (var alg = SHA256.Create())
+            {
+                return alg.ComputeHash(stream.ToArray());
+            }
+        }
+    }
+}
